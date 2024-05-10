29f97071e135aed65a93c06dc948022dd4609f38
l4blee <l4blee@yandex.ru>
2024-05-11 00:34:19 +0700

    hotfix

--- index.py
+++ index.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
 
     with client:
         client.loop.run_until_complete(uploads(client))
-        # client.loop.run_until_complete(notify_admin(client, 'The bot has started'))
-        client.loop.run_until_complete(client.send_message('l4blee', 'bot started'))
+        client.loop.run_until_complete(notify_admin(client, 'The bot has started'))
+        # client.loop.run_until_complete(client.send_message('l4blee', 'bot started'))
         client.run_until_disconnected()
 
